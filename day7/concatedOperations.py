4179115822d49496a6be5188efa01f6c014a37c0
Dootsire <fedczukpiotr@gmail.com>
2024-12-08 16:36:56 +0000

    day 7

---
+++ day7/concatedOperations.py
@@ -0,0 +1,48 @@
+totals = []
+components = []
+
+with open("data.txt","r") as file:
+    for line in file.readlines():
+        stripped_line = line.strip()
+        split_line = stripped_line.split(":")
+        totals.append(int(split_line[0]))
+        comps = []
+        for item in split_line[1].strip().split(" "):
+            comps.append(int(item))
+        components.append(comps)
+
+class Node:
+    def __init__(self, data):
+        self.right = None
+        self.left = None
+        self.middle = None
+        self.data = data
+
+def add_nodes(parent, list):
+    if len(list) > 0:
+        parent.left = Node(parent.data + list[0])
+        parent.right = Node(parent.data * list[0])
+        parent.middle = Node(int(str(parent.data) +str(list[0])))
+        add_nodes(parent.left, list[1:])
+        add_nodes(parent.right, list[1:])
+        add_nodes(parent.middle, list[1:])
+
+def search(node, number):
+    if (node.middle == None):
+        if (node.data == number):
+            return True
+        else:
+            return False
+    return (search(node.left, number) or search(node.right, number) 
+            or search(node.middle, number))
+
+output = 0
+i = 0
+for entry_list in components:
+    root = Node(entry_list[0])
+    add_nodes(root, entry_list[1:])
+    if search(root, totals[i]) == True:
+        output += totals[i]
+    i += 1
+    
+print(output)
